97dab1a81a5efa47995ebe4ca7207554111edea7
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-03-14 22:20:17 -0300

    criando a funcionalidade de tasks

--- src/components/Task/index.tsx
+++ src/components/Task/index.tsx
@@ -1,13 +1,34 @@
 import styles from './styles.module.css';
 import trash from '../../assets/trash.svg';
+import { ChangeEvent } from 'react';
 
-export function Task() {
+interface StatusProps {
+  id: string;
+  statusChange: boolean;
+}
+
+interface TaskProps {
+  id: string;
+  task: string;
+  onDeleteTask: (taskId: string) => void;
+  onStatusChange: ({ id, statusChange }: StatusProps) => void;
+}
+
+export function Task({ id, task, onDeleteTask, onStatusChange }: TaskProps) {
+  function handleDeleteTask() {
+    onDeleteTask(id);
+  }
+
+  function handleStatusTask(event: ChangeEvent<HTMLInputElement>) {
+    onStatusChange({ id, statusChange: event.target.checked });
+  }
+  
   return (
     <div className={styles.task}>
-      <input type='checkbox' />
-      <label>
-        Ir a academia
-        <button>
+      <input id={id} type='checkbox' onChange={handleStatusTask} />
+      <label htmlFor={id}>
+        {task}
+        <button onClick={handleDeleteTask}>
           <img src={trash} alt='Delete' />
         </button>
       </label>
