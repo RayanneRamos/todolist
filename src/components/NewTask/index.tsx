e48295237e19af4f724d7feb6e4957e24e476c66
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-03-14 22:20:00 -0300

    criando a funcionalidade de novas tarefas

--- src/components/NewTask/index.tsx
+++ src/components/NewTask/index.tsx
@@ -1,13 +1,39 @@
 import styles from './styles.module.css';
 import plusImage from '../../assets/plus.svg';
+import { ChangeEvent, FormEvent, useState } from 'react';
 
-export function NewTask() {
+interface CreateTask {
+  onCreate: (task: string) => void;
+}
+
+export function NewTask({ onCreate }: CreateTask) {
+  const [ newTask, setNewTask ] = useState('');
+
+  function handleCreateNewTask(event: FormEvent) {
+    event.preventDefault();
+
+    onCreate(newTask);
+    setNewTask('');
+  }
+
+  function handleNewStatusChange(event: ChangeEvent<HTMLInputElement>) {
+    event.target.setCustomValidity('');
+    setNewTask(event.target.value);
+  }
+
+  function handleNewStatusInvalid(event: ChangeEvent<HTMLInputElement>) {
+    event.target.setCustomValidity('Precisa adicionar uma tarefa');
+  }
+  
   return (
     <div className={styles.container}>
-      <form>
+      <form onSubmit={handleCreateNewTask}>
         <input 
         type='text' 
         placeholder='Adicione uma nova tarefa'
+        value={newTask}
+        onChange={handleNewStatusChange}
+        onInvalid={handleNewStatusInvalid}
         />
         <button type='submit'>
           Criar
